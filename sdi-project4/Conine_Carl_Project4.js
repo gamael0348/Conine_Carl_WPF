aba9832a658b2da8fb246e75f592d6f70b530f84
Carl Conine <cjconine@fullsail.edu>
2015-01-28 17:26:12 -0500

    Completed the rounding and title casing
    started working on them last night but I didn’t finish until today.

--- sdi-project4/Conine_Carl_Project4.js
+++ sdi-project4/Conine_Carl_Project4.js
@@ -11,15 +11,18 @@ var phoneNumber = function (){
   var myPrompt = prompt ("Please input a number to see if it is in valid phone number pattern", "Input a number.");
     var first = myPrompt.indexOf("-");
     if (myPrompt.length >= 13 || myPrompt.length < 12) {
-        nope()
+        nope();
+        return false
     }
     else if (first === -1){
-        nope()
+        nope();
+        return false
     }
     else if (first === 3){
         var second = myPrompt.lastIndexOf("-");
         if (second != 7){
-            nope()
+            nope();
+            return false
         }
         else if (second === 7){
             var firstSet = myPrompt.substring(0, 3);
@@ -28,11 +31,13 @@ var phoneNumber = function (){
             var wholeSet = firstSet + secondSet + thirdSet;
             parseInt(wholeSet);
             if (wholeSet <= 9999999999) {
-                yup()
+                yup();
+                return true
 
             }
             else {
-                nope()
+                nope();
+                return false
             }
         }
     }
@@ -40,18 +45,20 @@ var phoneNumber = function (){
 };
 //phoneNumber();
 
-
 // Is the string a URL? (Does it start with http: or https:?)
 var url = function(){
   var myPrompt = prompt("Please enter a URL.");
     if (myPrompt.search("http:") === 0){
-        yup()
+        yup();
+        return true
     }
     else if (myPrompt.search("https:") === 0){
-        yup()
+        yup();
+        return false
     }
     else {
-        nope()
+        nope();
+        return false
     }
 };
 // url();
@@ -63,7 +70,17 @@ var url = function(){
  of each word to uppercase while making sure the rest of the word is lowercase,
  and then returning the string from the function so it can be output.
  */
-
+var titleCasing = function(){
+    var string1 = prompt("Please input text.");
+    var array1 = string1.split(" ");
+    for (var i = 0; i < array1.length; i++){
+        array1[i] = array1[i].charAt(0).toUpperCase() + array1[i].substr(1);
+    }
+    var string2 = array1.join(" ");
+    alert(string2);
+    return string2
+};
+// titleCasing();
 
 // Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
 
@@ -74,14 +91,27 @@ var url = function(){
  send the following argument parameters: (3.14159, 3). The function will then
  return 3.142.
  */
-// Find the number of hours or days difference between two dates.
-/*
- This function requires you to send three parameters. The first two will be dates
- that you want to compare, and the third will be a string to determine if the
- function returns hours or days. The function will then perform the math
- necessary to find the difference between the two dates and return the choice of
- hours or days.
- */
+var roundingUp = function(myPrompt, myPrompt1){
+  //var myPrompt = prompt("Please input the number you wish to round.");
+    //var myPrompt1 = prompt("Please input the number of decimal places you want to round to.");
+    parseFloat(myPrompt);
+    if (isNaN(myPrompt) === true){
+        nope();
+        return false
+    }
+    parseInt(myPrompt1);
+    if (isNaN(myPrompt1) === true){
+        nope();
+        return false
+    }
+    var roundedNumber = Math.round(myPrompt*(Math.pow(10, myPrompt1)))/(Math.pow(10, myPrompt1));
+    alert(roundedNumber);
+    return roundedNumber
+};
+/*var myPrompt = prompt("Please input the number you wish to round.");
+var myPrompt1 = prompt("Please input the number of decimal places you want to round to.");
+roundingUp(myPrompt, myPrompt1);
+*/
 
 // Given a string version of a number such as "42",
 // return the value as an actual Number, such as 42.
@@ -96,15 +126,20 @@ var notANumber = function(){
     var myPrompt = prompt("Please input a number.");
     parseInt(myPrompt);
     if (isNaN(myPrompt) === true){
-        nope();}
+        nope();
+        return false
+    }
         else if (isNaN(myPrompt)=== false){
-            alert(myPrompt)
+            alert(myPrompt);
+        return myPrompt
 
         }
 
 
 };
-notANumber();
+// notANumber();
+
+
 // Find the total value of just the numbers in an array, even if some of the
 // items are not numbers.
 /*
@@ -114,4 +149,7 @@ notANumber();
  function should return 15. Therefore, it should be able to recognize “10” as a
  string and ignore it from the total.
  */
+var addTheArray = function(){
 
+};
+addTheArray();
